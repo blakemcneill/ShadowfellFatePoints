e5099ce309dad70c6e4c65374f8d362ca304a149
Blake McNeill <blakemcneill@gmail.com>
2023-04-12 23:29:51 -0400

    Updated button generaton

--- app.py
+++ app.py
@@ -18,25 +18,29 @@ characters = Table(
 
 app = Flask(__name__)
 
-plus_button = """
-<button class="btn btn-primary">
-   +
-</button>
-"""
 
-minus_button = """
-<button class="btn btn-danger">
-   -
-</button>
-"""
+def generate_button(button_type, player_id):
+    if button_type.lower() == "add":
+        return """
+            <button class="btn btn-primary">
+               +
+            </button>
+        """
+    if button_type.lower() == "subtract":
+        return """
+            <button class="btn btn-danger">
+               -
+            </button>
+        """
+    if button_type.lower() == "delete":
+        return """
+            <button class="btn btn-secondary">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                </svg>
+            </button>
+        """
 
-delete_button = """
-<button class="btn btn-secondary">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-      <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-    </svg>
-</button>
-"""
 
 @app.route('/')
 def index():
@@ -48,9 +52,9 @@ def index():
             c_builder = '<div class="container-left text-left">'
             for row in conn.execute(stmt):
                 c_builder += "<div class = \"row\">"
-                c_builder += f"<div class = \"col\"><h1>{row.charactername}</h1>{delete_button}</div>"
-                c_builder += f"<div class=\"col\"><h3>Heroic Points</h3><br />{row.heroicpoints}{plus_button}{minus_button}</div>"
-                c_builder += f"<div class=\"col\"><h3>Villain Points</h3><br />{row.villianpoints}{plus_button}{minus_button}</div>"
+                c_builder += f"<div class = \"col\"><h1>{row.charactername}</h1>{generate_button('delete', 0)}</div>"
+                c_builder += f"<div class=\"col\"><h3>Heroic Points</h3><br />{row.heroicpoints}{generate_button('add', 0)}{generate_button('subtract', 0)}</div>"
+                c_builder += f"<div class=\"col\"><h3>Villain Points</h3><br />{row.villianpoints}{generate_button('add', 0)}{generate_button('subtract', 0)}</div>"
                 c_builder += '</div>'
             c_builder += "</div>"
     return render_template('index.html', character_display=c_builder)
